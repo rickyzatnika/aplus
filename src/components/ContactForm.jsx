023e43cb944ed0061af4afb6b02d8a7de1e2f129
rickyzatnika <rickyzatnika91@gmail.com>
2023-08-24 22:30:30 +0700

    ampas

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,8 +6,8 @@ const ContactForm = ({ openForm, setOpenForm }) => {
 
 
   return (
-    <div className={`${openForm ? "right-0" : "-right-[100%]"} overflow-hidden max-h-screen w-full bg-transparent fixed top-0 -right-[100%] backdrop-blur-sm  z-[99] transition-all duration-200 ease-linear `}>
-      <div className=" py-2 px-2 transition-all duration-300 ease-linear w-[90%] lg:w-[70%]  bg-white h-full">
+    <div className={`${openForm ? "w-full " : "w-0 "} overflow-hidden max-h-screen w-full bg-transparent fixed right-0 origin-top-right backdrop-blur-sm  z-[99] transition-all duration-200 ease-linear `}>
+      <div className=" py-2 px-2 transition-all duration-300 ease-linear w-[90%] lg:w-[70%] ml-auto bg-white h-full">
         <div className='relative w-full h-screen flex flex-col items-start justify-between'>
           <div className="w-full">
             <div className="flex item-center justify-between w-full px-4 py-2">
