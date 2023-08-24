355b03706112f951a47fc84028e461317fb6f0a1
ricyzatnika <ricyzatnika91@gmail.com>
2023-08-24 19:41:44 +0700

    add contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client"
 
-const ContactForm = () => {
-    return (
-        <div>ContactForm</div>
-    )
+import { useState, useEffect } from 'react'
+
+const ContactForm = ({ openForm, setOpenForm }) => {
+
+
+  return (
+    <div className={`${openForm ? "right-0" : "-right-[100%]"} overflow-hidden min-h-screen w-full bg-transparent fixed top-0 backdrop-blur-sm right-0 z-[99] transition-all duration-200 ease-linear `}>
+      <div className=" py-2 px-2 transition-all duration-300 ease-linear w-[90%] lg:w-[70%] absolute top-0 right-0 bg-white h-full">
+        <button className="absolute top-6 right-6 text-xl" onClick={() => setOpenForm(false)}>X</button>
+        <div className='relative w-full h-screen flex flex-col items-start justify-between'>
+          <div>
+            <h2 className='text-md lg:text-lg '>Become a Client</h2>
+            <form className='w-full px-6 py-4'>
+              <div className='flex flex-col gap-2 items-start'>
+                <label>Company Name :</label>
+                <input type="text" className='px-6 py-2 w-full outline-none border rounded-md border-gray-500 focus:border-gray-700' />
+              </div>
+            </form>
+          </div>
+          <button>Submit</button>
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 export default ContactForm
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,15 @@ import Image from "next/image"
 import Link from "next/link"
 import { useState, useEffect } from "react"
 import NavMobile from "./NavMobile"
+import ContactForm from "./ContactForm"
 
 const Navbar = () => {
 
   const [scroll, setScroll] = useState(false);
 
   const [open, setOpen] = useState(false);
+  const [openForm, setOpenForm] = useState(false);
+
   const handleBurger = () => {
     setOpen(!open);
   };
@@ -21,80 +24,84 @@ const Navbar = () => {
   }, []);
 
   return (
-    <header>
-      <nav className={` px-8 py-5 w-full z-50 fixed top-0 left-0 flex items-center justify-between  ${scroll ? "bg-[#121212] transition-all duration-300 ease-linear" : "transition-all duration-300 ease-linear"}  `}>
-        <div className="relative z-50 flex flex-col items-center justify-center">
-          <Link href="/" className="flex items-center">
-            <Image alt="" src={`${scroll ? "/logo_red.png" : "/logo.png"}`} width={40} height={40} className="object-cover" />
-            <h1 className="text-zinc-100 uppercase text-xl">Aplus</h1>
-          </Link>
-        </div>
-        <ul className={`${scroll ? "bg-white text-zinc-500" : "bg-transparent text-white"}  px-12 hidden lg:relative py-3 rounded-full lg:flex gap-8 `}>
-          <li className="flex items-center justify-center">
-            <Link href="/about" className="group overflow-hidden h-[22px] px-2 ">
-              <div className="flex flex-col items-center group-hover:-translate-y-5 transition-all duration-500 ease ">
-                <span className="text-sm">About Us</span>
-                <span className="text-sm">About Us</span>
-              </div>
+    <>
+      <ContactForm openForm={openForm} setOpenForm={setOpenForm} />
+      <header>
+        <nav className={` px-6 lg:px-8 py-5 w-full z-50 fixed top-0 left-0 flex items-center justify-between  ${scroll ? "bg-[#121212] transition-all duration-300 ease-linear" : "transition-all duration-300 ease-linear"}  `}>
+          <div className="relative z-50 flex flex-col items-center justify-center">
+            <Link href="/" className="flex items-center">
+              <Image alt="" src={`${scroll ? "/logo_red.png" : "/logo.png"}`} width={40} height={40} className="object-cover" />
+              <h1 className="text-zinc-100 uppercase text-xl">Aplus</h1>
             </Link>
-          </li>
-          <li className="flex items-center justify-center">
-            <Link href="/about" className="group overflow-hidden h-[22px] px-2 ">
-              <div className="flex flex-col items-center group-hover:-translate-y-5 transition-all duration-500 ease ">
-                <span className="text-sm">Project</span>
-                <span className="text-sm">Project</span>
-              </div>
-            </Link>
-          </li>
-          <li className="flex items-center justify-center">
-            <Link href="/about" className="group overflow-hidden h-[22px] px-2 ">
-              <div className="flex flex-col items-center group-hover:-translate-y-5 transition-all duration-500 ease ">
-                <span className="text-sm">Portfolio</span>
-                <span className="text-sm">Portfolio</span>
-              </div>
-            </Link>
-          </li>
-          <li className="flex items-center justify-center">
-            <Link href="/about" className="group overflow-hidden h-[22px] px-2 ">
-              <div className="flex flex-col items-center group-hover:-translate-y-5 transition-all duration-500 ease ">
-                <span className="text-sm">Contact Us</span>
-                <span className="text-sm">Contact Us</span>
-              </div>
-            </Link>
-          </li>
-        </ul>
-        <NavMobile open={open} setOpen={setOpen} />
-        <div div className="flex items-center gap-4">
-          <button className={`btc_container ${scroll ? "bg-gradient-to-r from-red-500 to-orange-500 text-zinc-100  transition-all duration-200 ease-linear" : "bg-white"} group px-4 py-2 bg-white rounded-full  flex items-center justify-center`}>
-            <div className="group overflow-hidden h-[22px]">
-              <div className="flex flex-col items-center group-hover:-translate-y-5 transition-all duration-200 ease-linear">
-                <span className="text-sm">
-                  + Become a Client</span>
-                <span className="text-sm">
-                  + Become a Client</span>
-              </div>
-            </div>
-          </button>
-          <div
-            className="rounded-md z-[9999] cursor-pointer top-0 flex lg:hidden relative  gap-1.5 flex-col justify-between items-end"
-            onClick={handleBurger}
-          >
-            <span
-              className={`h-0.5 w-6 bg-blend-color origin-center rounded-lg  transform transition duration-300 ease-out ${open ? "rotate-45 translate-y-2 bg-white " : "bg-white "
-                }`}
-            ></span>
-            <span
-              className={`h-0.5 w-4 bg-blend-color  bg-red-600 rounded-lg transform transition duration-500 ease-out ${open ? "opacity-0" : ""
-                }`}
-            ></span>
-            <span
-              className={`h-0.5 w-6 bg-blend-color origin-center rounded-lg  transform transition duration-300  ease-out ${open ? "-rotate-45 -translate-y-2  bg-white " : "bg-white "
-                }`}
-            ></span>
           </div>
-        </div>
-      </nav>
-    </header>
+          <ul className={`${scroll ? "bg-white text-zinc-500" : "bg-transparent text-white"}  px-12 hidden lg:relative py-3 rounded-full lg:flex gap-8 `}>
+            <li className="flex items-center justify-center">
+              <Link href="/about" className="group overflow-hidden h-[22px] px-2 ">
+                <div className="flex flex-col items-center group-hover:-translate-y-5 transition-all duration-500 ease ">
+                  <span className="text-sm">About Us</span>
+                  <span className="text-sm">About Us</span>
+                </div>
+              </Link>
+            </li>
+            <li className="flex items-center justify-center">
+              <Link href="/about" className="group overflow-hidden h-[22px] px-2 ">
+                <div className="flex flex-col items-center group-hover:-translate-y-5 transition-all duration-500 ease ">
+                  <span className="text-sm">Project</span>
+                  <span className="text-sm">Project</span>
+                </div>
+              </Link>
+            </li>
+            <li className="flex items-center justify-center">
+              <Link href="/about" className="group overflow-hidden h-[22px] px-2 ">
+                <div className="flex flex-col items-center group-hover:-translate-y-5 transition-all duration-500 ease ">
+                  <span className="text-sm">Portfolio</span>
+                  <span className="text-sm">Portfolio</span>
+                </div>
+              </Link>
+            </li>
+            <li className="flex items-center justify-center">
+              <Link href="/about" className="group overflow-hidden h-[22px] px-2 ">
+                <div className="flex flex-col items-center group-hover:-translate-y-5 transition-all duration-500 ease ">
+                  <span className="text-sm">Contact Us</span>
+                  <span className="text-sm">Contact Us</span>
+                </div>
+              </Link>
+            </li>
+          </ul>
+          <NavMobile open={open} setOpen={setOpen} />
+          <div div className="flex items-center gap-4">
+            <button onClick={() => setOpenForm(true)} className={`btc_container ${scroll ? "bg-gradient-to-r from-red-500 to-orange-500 text-zinc-100  transition-all duration-200 ease-linear" : "bg-white"} group px-4 py-2 bg-white rounded-full  flex items-center justify-center`}>
+              <div className="group overflow-hidden h-[22px]">
+                <div className="flex flex-col items-center group-hover:-translate-y-5 transition-all duration-200 ease-linear">
+                  <span className="text-sm">
+                    + Become a Client</span>
+                  <span className="text-sm">
+                    + Become a Client</span>
+                </div>
+              </div>
+            </button>
+            <div
+              className="rounded-md z-[9999] cursor-pointer top-0 flex lg:hidden relative  gap-1.5 flex-col justify-between items-end"
+              onClick={handleBurger}
+            >
+              <span
+                className={`h-0.5 w-6 bg-blend-color origin-center rounded-lg  transform transition duration-300 ease-out ${open ? "rotate-45 translate-y-2 bg-white " : "bg-white "
+                  }`}
+              ></span>
+              <span
+                className={`h-0.5 w-4 bg-blend-color  bg-red-600 rounded-lg transform transition duration-500 ease-out ${open ? "opacity-0" : ""
+                  }`}
+              ></span>
+              <span
+                className={`h-0.5 w-6 bg-blend-color origin-center rounded-lg  transform transition duration-300  ease-out ${open ? "-rotate-45 -translate-y-2  bg-white " : "bg-white "
+                  }`}
+              ></span>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+
   )
 }
 
