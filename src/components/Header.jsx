2219fd461e8adb9653c8877d942a6fbaf6f59277
ricyzatnika <ricyzatnika91@gmail.com>
2023-08-24 17:23:40 +0700

    mobile responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,17 @@ const Header = () => {
       </div>
       <div className='absolute z-20 w-full h-full bottom-0 bg-gradient-to-tr from-black to-transparent ' />
       <div className='relative z-30 grid grid-cols-1 lg:grid-cols-12 '>
-        <div className="col-span-1 lg:col-span-8 py-32 px-12">
+        <div className="col-span-1 lg:col-span-8 py-20 px-8 lg:py-32 lg:px-12">
           <div className="flex pb-8 leading-relaxed flex-col w-full  item-center justify-center gap-3 text-zinc-100">
             <h1 className='text-md lg:text-xl'>Multicreation Company</h1>
             <h3 className="text-3xl lg:text-5xl bg-gradient-to-tr tracking-widest from-red-600 to-orange-500 bg-clip-text text-transparent font-extrabold uppercase">inspiring partner</h3>
-            <p className='w-full lg:w-[60%] text-md '>Aplus Multicreation are a creative agency in the marketing sphere that focuses on Event, Production and Branding.</p>
+            <p className='w-full lg:w-[60%] leading-relaxed text-sm lg:text-md '>Aplus Multicreation are a creative agency in the marketing sphere that focuses on Event, Production and Branding.</p>
           </div>
           <button className='px-6 py-2 bg-gradient-to-tr from-red-600 to-orange-500 text-zinc-200 text-md'>
             <Link href="/about">Read More</Link>
           </button>
         </div>
-        <div className="col-span-1 lg:col-span-4">
+        <div className="col-span-1 lg:col-span-4 text-white flex flex-row lg:flex-col items-center justify-center">
           <h3>sosmed</h3>
         </div>
       </div>
