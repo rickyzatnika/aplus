d29ebc787a48c9ac5597d8bb007a7fe65f5e3311
rickyzatnika <rickyzatnika91@gmail.com>
2023-08-28 09:54:38 +0700

    fix API url

--- src/app/projects/[id]/page.jsx
+++ src/app/projects/[id]/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 async function getData(id) {
     const res = await fetch(
-        `/api/services/${id}`,
+        `${process.env.NEXT_PUBLIC_API}/api/services/${id}`,
         {
             cache: "no-store", // this will fresh data on every fetch request;
             // next: { revalidate: 10 }, // and this , will be refresh data every 10 seconds;
